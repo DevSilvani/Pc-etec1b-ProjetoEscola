cd382805f142d0bae91dc43f4fc12db2f816f5d4
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 11:29:09 -0300

    C# pronto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pc_etec1b_ProjetoEscola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ETEC Adolfo Berezin");
+            Console.ReadKey();
+        }
+    }
+}
